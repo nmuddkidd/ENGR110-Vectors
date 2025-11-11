29c87c984276bc32d9b48feafd839e7107759475
nmuddkidd <Nmuddkidd543@gmail.com>
2025-11-11 13:31:24 -0500

    basic menu

--- Vector proj.py
+++ Vector proj.py
@@ -1,1 +1,32 @@
-print("Hello world!")
+def conversion():
+    print("conversion")
+    deg = int(input("input degrees: ")) + int(input("input minutes: "))/60 + int(input("input seconds: "))/3600
+    print(deg,"are the decimal degrees")
+    input("press enter to return to menu")
+
+def directconversion(degrees, minutes, seconds):
+    return degrees+minutes/60+seconds/3600;
+1
+def distancecalc():
+    print("distance calc")
+    input("press enter to return to menu")
+
+def haversinecalc():
+    print("haversine calc")
+    input("press enter to return to menu")
+
+choice = -1;
+while choice != 0:
+    print("MENU:")
+    print("0) exit")
+    print("1) Geographic coordinate conversion")
+    print("2) Resultant vector calculation (distance)")
+    print("3) Resultant vector calculation (haversine)")
+    choice = int(input())
+    match choice:
+        case 1:
+            conversion()
+        case 2:
+            distancecalc()
+        case 3:
+            haversinecalc()
---
+++ vectordisplay.pde
@@ -0,0 +1,33 @@
+void setup(){
+  frameRate(40);
+  size(500,500);
+  background(255,255,255);
+}
+
+void draw(){
+  float x=250;
+  float y=50;
+  circle(x,y,5);
+  y-=-13.34272523 ; x+=18.36468577;
+  circle(x,y,2);
+  y-=-49.6786897  ;x+=-3.473872392;
+  circle(x,y,2);
+  y-=-55.54356382 ;x+= 24.7295879;
+  circle(x,y,2);
+  y-=-17.16107775 ; x+=-11.5752931;
+  circle(x,y,2);
+  y-=2.811296639  ;x+=-20.00341499;
+  circle(x,y,2);
+  y-=-18.88782168 ; x+=-6.874604881;
+  circle(x,y,2);
+  y-=5.407797402  ;x+=-16.64348904;
+  circle(x,y,2);
+  line(250,50,x,y);
+  println("xdist: "+(x-250)+" ydist: "+(y-50));
+  println("dist: "+sqrt(pow(x-250,2)+pow(y-50,2)));
+  println("angle: "+theta(x-250,y-50));
+}
+
+float theta(float x, float y){
+    return X-x>=0?atan((Y-y)/(X-x))+PI:atan((Y-y)/(X-x));
+}
